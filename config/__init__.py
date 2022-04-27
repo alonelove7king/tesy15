d4a1b17685484020c258ec7a379e2bd76ec74bbc
kimoland <85030146+kimoland@users.noreply.github.com>
2022-04-27 22:53:38 +0430

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = int( os.getenv("api_id","1234") )
-    API_HASH = os.getenv("api_hash","21ab7cb0a453b5e60016dc7bbeb71cb")
-    CHANNEL = int( os.getenv("channel_files_chat_id","-1001601419165") )
-    CHANNEL_USERNAME = os.getenv("channel_username","UserLandapp")
-    TOKEN = os.getenv("token","1408542385:AAEatXPbSubsYmlW8kPTUAr")
-    DOMAIN  = os.getenv("domain","http://localhost")
+    API_ID = int( os.getenv("api_id","16612056") )
+    API_HASH = os.getenv("api_hash","ae32caf162207865ff93b9b931b2ba54")
+    CHANNEL = int( os.getenv("channel_files_chat_id","-1001792892962") )
+    CHANNEL_USERNAME = os.getenv("channel_username","forjoim777888")
+    TOKEN = os.getenv("token","5305336110:AAHyDsa9_exGaOTyv8qDxaw_U0Z1BOO-zYM")
+    DOMAIN  = os.getenv("domain","http://d4.kimo.vip")
