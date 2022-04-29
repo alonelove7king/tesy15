2437554939b2ec627ee211c5f614e8dcdd2383a4
alonelove7 <34532101+alonelove7@users.noreply.github.com>
2022-04-30 02:11:25 +0430

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -6,9 +6,9 @@ import asyncio
 import threading
 import re
 from urllib.parse import quote
+w = dict()
+v = dict()
 import time
-
-
 
 client = TelegramClient(
             StringSession(),
@@ -16,8 +16,7 @@ client = TelegramClient(
             Config.API_HASH,
             # proxy=("socks5","127.0.0.1",9050)
             ).start(bot_token=Config.TOKEN)
-w = dict()
-v = dict()
+
 username_bot = client.get_me().username
 
 def get_file_name(message):
@@ -43,55 +42,74 @@ async def download(event):
             except errors.UserNotParticipantError:
                 await event.reply(f"🌀برای استفاده از ربات ابتدا باید در کانال ما عضو بشی\n💠برای عضویت روی ایدی زیر کلیک کن سپس دستور /start رو ارسال کن\n\n🔸@{Config.CHANNEL_USERNAME}")
                 return
-        if event.file :
-            if not pv :
-                if not event.file.size > 10_000_000:
+            if event.file :
+                if not pv :
+                    if not event.file.size > 10_000_000:
                         return 
                 sender = await event.get_sender()
-            msg = await event.client.send_file(
-                Config.CHANNEL,
-                 file=event.message.media,
+                msg = await event.client.send_file(
+                    Config.CHANNEL,
+                    file=event.message.media,
                     caption=f"@{sender.username}|[{event.chat_id}](tg://user?id={event.sender_id})/{event.message.id}")
-            id_hex = hex(msg.id)[2:]
-            id = f"{id_hex}/@{Config.CHANNEL_USERNAME}-{get_file_name(msg)}"
-            bot_url = f"t.me/{username_bot}?start={id_hex}"
-            await event.reply(f"✅فایل شما با موفقیت به لینک تبدیل شد\n\n🌐 Link : {Config.DOMAIN}/{id}\n\n⚠️لینک های دانلود نیم بها میباشد، لذا قبل از دانلود فیلترشکن خود را خاموش کنید!\n\n‼️فایل های ارسالی بعد از 1 روز از روی سرور ها پاک مشوند‼️\n\n🆔 @{Config.CHANNEL_USERNAME}",link_preview=False)
-            return
-        elif id_msg := re.search("/start (.*)", event.raw_text ):
-            if id_hex := id_msg.group(1) :
-                try:
-                    id = int(id_hex,16)
-                except ValueError:
-                    return
-                msg = await event.client.get_messages(Config.CHANNEL,ids=id)
-                if not msg or not msg.file :
-                    return await event.reply("404! File Not Found")
-                if regex := re.search(r"(\d*)/(\d*)",msg.message):
-                    if user_id := int(regex.group(1)) :
-                        msg_id = int(regex.group(2))
-                        file = await event.client.get_messages(user_id,ids=msg_id)
-                        if not file or not file.file :
-                            return await event.reply("404! File Not Found")
-                        forward = await file.forward_to(event.chat_id)
-                        id_name = f"{id_hex}/{get_file_name(msg)}"
-                        bot_url = f"t.me/{username_bot}?start={id_hex}"
-                        forward_reply = await forward.reply(f"will be deleted in 21 second. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=False)
-                        await asyncio.sleep(12)
-                        await forward_reply.edit(f"will be deleted in 10 second. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}")
-                        await asyncio.sleep(10)
-                        await forward.delete()
-                        await forward_reply.edit(f"📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=True)
+                id_hex = hex(msg.id)[2:]
+                id = f"{id_hex}/{get_file_name(msg)}"
+                bot_url = f"[share](t.me/{username_bot}?start={id_hex})"
+                await event.reply(f"✅فایل شما با موفقیت به لینک تبدیل شد\n\n🌐 Link : {Config.DOMAIN}/{id}\n\n⚠️لینک های دانلود نیم بها میباشد، لذا قبل از دانلود فیلترشکن خود را خاموش کنید!\n\n‼️فایل های ارسالی بعد از 1 روز از روی سرور ها پاک مشوند‼️\n\n🆔 @{Config.CHANNEL_USERNAME_TW}",link_preview=False)
                 return
-        if pv:
-            await event.reply(f"🌀خوش آمدید\n🔰برای استفاده از ربات کافی است\nفایل خود را ارسال کرده و سپس لینک آن را دریافت کنید\n\n🆔 @{Config.CHANNEL_USERNAME}")
-    elif event.is_channel:
-        if event.chat_id == Config.CHANNEL:
-            if event.reply_to:
-                msg = await event.get_reply_message()
-                if regex := re.search(r"(\d*)/(\d*)",msg.message):
-                    if user_id := int(regex.group(1)) :
-                        msg_id = int(regex.group(2))
-                        if await event.client.send_message(entity=user_id, message=event.message, reply_to=msg_id):
-                            await event.client.edit_message("@King_Network7")
-   
+        
+            elif id_msg := re.search("/start (.*)", event.raw_text ):
+                try :
+                    if id_hex := id_msg.group(1) :
+                        try:
+                            id = int(id_hex,16)
+                        except ValueError:
+                            return
+                        msg = await event.client.get_messages(Config.CHANNEL,ids=id)
+                        if not msg or not msg.file :
+                            return await event.reply("404! File Not Found")
+                        if regex := re.search(r"(\d*)/(\d*)",msg.message):
+                            if regex.group(1) :
+                                user_id = int(regex.group(1))
+                                if "100" in regex.group(1):
+                                    user_id = int("-"+regex.group(1))
+                                msg_id = int(regex.group(2))
+                                file = await event.client.get_messages(user_id,ids=msg_id)
+                                if not file or not file.file :
+                                    return await event.reply("404! File Not Found")
+                                forward = await file.forward_to(event.chat_id)
+                                id_name = f"{id_hex}/{get_file_name(msg)}"
+                                bot_url = f"[share](t.me/{username_bot}?start={id_hex})"
+                                forward_reply = await forward.reply(f"will be deleted in 21 seconds. \n\n📎 : [Link]({Config.DOMAIN}/{id_name})\n🤖 : {bot_url}",link_preview=False)
+                                await asyncio.sleep(12)
+                                await forward_reply.edit(f"will be deleted in 10 seconds. \n\n📎 : [Link]({Config.DOMAIN}/{id_name})\n🤖 : {bot_url}")
+                                await asyncio.sleep(10)
+                                await forward.delete()
+                                await forward_reply.edit(f"📎 : [Link]({Config.DOMAIN}/{id_name})\n🤖 : {bot_url}",link_preview=True)
+                        return
+                except:
+                    return await event.reply("404! فایل یافت نشد")
+            
+            if pv:
+                #if event.raw_text == "/start":
+                    await event.reply("🌀خوش آمدید\n🔰برای استفاده از ربات کافی است\nفایل خود را ارسال کرده و سپس لینک آن را دریافت کنید")
+                #else :
+                    #await event.delete()
+                
+            
+        
+        elif event.is_channel:
+            if event.chat_id == Config.CHANNEL:
+                if event.reply_to:
+                    msg = await event.get_reply_message()
+                    if regex := re.search(r"(\d*)/(\d*)",msg.message):
+                        if regex.group(1) :
+                            user_id = int(regex.group(1))
+                            if "100" in regex.group(1):
+                                user_id = int("-"+regex.group(1))
+                            msg_id = int(regex.group(2))
+                            if await event.client.send_message(entity=user_id, message=event.message, reply_to=msg_id):
+                                await event.client.edit_message(event.chat_id,event.id,f"{event.message.message}\n sended")
+                            
+                        
+                    
 client.run_until_disconnected()
