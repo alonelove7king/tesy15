86ff80188efeff7ec5db56ece988c36f747c9d7d
alonelove7 <34532101+alonelove7@users.noreply.github.com>
2022-05-04 00:18:54 +0430

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -52,7 +52,7 @@ async def download(event):
                     file=event.message.media,
                     caption=f"@{sender.username}|[{event.chat_id}](tg://user?id={event.sender_id})/{event.message.id}")
                 id_hex = hex(msg.id)[2:]
-                id = f"{id_hex}/{get_file_name(msg)}"
+                id = f"{id_hex}/@{Config.CHANNEL_USERNAME}-{get_file_name(msg)}"
                 bot_url = f"[share](t.me/{username_bot}?start={id_hex})"
                 await event.reply(f"✅فایل شما با موفقیت به لینک تبدیل شد\n\n🌐 Link : {Config.DOMAIN}/{id}\n\n⚠️لینک های دانلود نیم بها میباشد، لذا قبل از دانلود فیلترشکن خود را خاموش کنید!\n\n‼️فایل های ارسالی بعد از 1 روز از روی سرور ها پاک مشوند‼️\n\n🆔 @{Config.CHANNEL_USERNAME}",link_preview=False)
                 return
